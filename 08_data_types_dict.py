4a80497e12fb291088adad5e96175289c204f389
Shavgath Perumal <48409809+shavgath@users.noreply.github.com>
2019-07-15 16:08:41 +0100

    completed data types dictionary

--- 08_data_types_dict.py
+++ 08_data_types_dict.py
@@ -1,2 +1,71 @@
 # Dictionaries AKA Hashes
+# work with key: value pairs.
+# as opposed to using indexes
+
+# Declaring a hash / dictionary
+pika = { }
+
+# Dictionaries work with keys: values
+pika = {
+    'name': 'Derik Dice',
+    'pokemon': 'pikachu',
+    'age': 17,
+    'personality': 'Grumpy'
+}
+
+print(pika)
+print(type(pika))
+# Access information using the keys
+print(pika['age'])
+print(pika['pokemon'])
+
+#Re assign values, using the keys
+pika['age'] = 18
+print(pika['age'])
+
+# Adding a key: value pair
+pika['colour'] = 'Yellowish'
+print(pika)
+
+# Creating key value for first & last name
+full_name = pika['name'].split()
+print(full_name)
+first_name = full_name[0]
+last_name = full_name[1]
+print(first_name + ' ' + last_name)
+pika['first_name'] = first_name
+pika['last_name'] = last_name
+print(pika)
+
+# Embedded data
+pika = {
+    'name': 'Derik Dice',
+    'pokemon': 'pikachu',
+    'age': 17,
+    'personality': ['grumpy', 'jumpy', 'shocking', 'static']
+}
+print(pika['personality'])
+print(pika['personality'][0])
+
+pika = {
+    'name': 'Derik Dice',
+    'pokemon': 'pikachu',
+    'age': 17,
+    'personality': {
+        'grumpy': 10,
+        'lovely': 2
+    }
+}
+
+print(pika['personality']['grumpy'])
+
+# Important Methods
+# Accessing keys (name)
+print(pika.keys())
+print(type(pika.keys()))
+print(pika.keys())
+
+# Accessing values
+values = pika.values()
+print(values)
 
