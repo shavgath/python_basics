26b40b02dae18651e4fd27988befd6a262343e71
Shavgath Perumal <48409809+shavgath@users.noreply.github.com>
2019-07-15 10:09:11 +0100

    Variables updated, initial data types

--- 01_variables.py
+++ 01_variables.py
@@ -12,3 +12,9 @@ print(my_variable)
 # Re-assigning a variable
 my_variable = "I'm new content being re-assigned to my_variable"
 print(my_variable)
+
+
+#Where is the variable/box stored?
+correct_response = 'CPU Registers'
+print('the correct response was', correct_response)
+print('the correct response was' + ' ' + correct_response)
