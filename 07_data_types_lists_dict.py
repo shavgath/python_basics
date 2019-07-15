8a6f7c683ce5f035812de7cc52b9a1fa7c3943ab
Shavgath Perumal <48409809+shavgath@users.noreply.github.com>
2019-07-15 15:14:23 +0100

    Completed lists/arrays

---
+++ 07_data_types_lists_dict.py
@@ -0,0 +1,77 @@
+# Lists and Dictionaries
+# List aka: array
+
+# Sometimes we just need to list our crazy x-pokemons :
+# because we don't want to work there
+
+# this is how you make a list
+# [] separate items using ,
+# ['bananas', 'pine', 'gasoline']
+# declaring a list and saving to variable
+crazy_pokemons = ['Snorlax', 'Jigglipuff', 'Mewtoo']
+
+print(crazy_pokemons)
+print(type(crazy_pokemons))
+# Lists are organised using index
+# [    0     ,   1   ,      2    ]
+# ['bananas', 'pine', 'gasoline']
+# Indexes start at 0
+
+print(len(crazy_pokemons))
+print(crazy_pokemons[2])
+# print first in the list
+print(crazy_pokemons[0])
+
+# If you want to print the last in a list
+# you have two options
+    # array[len(array) - 1]
+print(crazy_pokemons[len(crazy_pokemons)-1]) # OR
+print(crazy_pokemons[-1])
+
+# Re-assigning the value in a list, using the index
+# We need to evolve Mewtoo to Mewtree
+print(crazy_pokemons)
+crazy_pokemons[2] = 'Mewtree'
+print(crazy_pokemons)
+
+# Appending a new pokemon
+# We caught Pigeotto
+crazy_pokemons.append('Pigeotto')
+print(crazy_pokemons)
+
+# Inserting with index numbers
+crazy_pokemons.insert(0, 'Rattata')
+print(crazy_pokemons)
+crazy_pokemons.insert(2, 'Pikachu')
+print(crazy_pokemons)
+
+# Removing a record - using index
+# removes item from last index
+crazy_pokemons.pop()
+print(crazy_pokemons)
+
+#removes item in first index
+crazy_pokemons.pop(0)
+print(crazy_pokemons)
+
+# Removing - using a value
+crazy_pokemons.remove('Snorlax')
+print(crazy_pokemons)
+
+# List can have any data types
+
+mixed_list = ['Jones', 10, 42, 'John']
+print(mixed_list)
+print(type(mixed_list[0]), type(mixed_list[1]))
+
+# Inception list
+leo_d = ['first', 2, ['leo', 'd']]
+
+print(leo_d[0][1])
+print('accessing the index 2')
+print(leo_d[2])
+print(leo_d[2][1])
+
+
+
+
