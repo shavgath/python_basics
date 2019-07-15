645094f3d31428b22345dd0a01ecb5c4ce47dc52
Shavgath Perumal <48409809+shavgath@users.noreply.github.com>
2019-07-15 12:01:22 +0100

    did the integers and numericals

---
+++ 03_data_types_ints.py
@@ -0,0 +1,60 @@
+# Numericals - Integers, Big Integers, long numbers, Complex numbers
+
+# Integers - Whole numbers
+my_int = 13
+print(type(13)) # NOT the same as print(type('13)) --> STRING!
+print(type(my_int))
+
+# Floats are composite numbers
+print(13.1)
+print(type(13.1))
+
+# Long and complex
+# Less used check syntax when needed
+
+# Operators
+    #Add, Subtract, Multiply, Divide
+num_a = 10
+num_b = 7.3
+print(num_a + num_b)
+print(num_a - num_b)
+print(num_a * num_b)
+print(num_a / num_b)
+
+#Modulus --> Check for remainders
+print(10 % 2)
+#checking if even
+print((12 % 2) == 0)
+print(18 % 3)
+
+# Logical Operators
+# evaluate two sides and return a boolean
+# Check if equal ==
+a = 10
+b = 13
+print(a == b)
+print(a == 10)
+
+# Checking if greater than or smaller than
+print(a > b)
+print(a > 9)
+print('.....')
+
+print(b < a)
+print(b < 15)
+
+# Checking if bigger/smaller or equal to
+print(a > 10)
+print(a >= 10)
+
+# Check if not something
+print(a != 9) # a is not 9, it is 10. so it returns true
+
+# Increment
+counter = 0
+counter += 1
+print(counter)
+counter += 1 
+print(counter)
+
+
