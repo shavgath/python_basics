88c16d60d9b09d5f97012a63174c02ac748340d7
Shavgath Perumal <48409809+shavgath@users.noreply.github.com>
2019-07-15 10:01:29 +0100

    example of variables done

---
+++ 01variables.py
@@ -0,0 +1,14 @@
+# Variables in Python
+
+
+# Its's like a box
+    # Has a name
+    # Stores things inside
+my_variable = 'Hello there'
+
+# Variable with a string being printed to console
+print(my_variable)
+
+# Re-assigning a variable
+my_variable = "I'm new content being re-assigned to my_variable"
+print(my_variable)
