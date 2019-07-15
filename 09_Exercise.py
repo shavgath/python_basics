5d008022d6d6ebd667181a387982ced5ac403e8f
Shavgath Perumal <48409809+shavgath@users.noreply.github.com>
2019-07-15 16:48:42 +0100

    Completed python basics exercise

---
+++ 09_Exercise.py
@@ -0,0 +1,42 @@
+sparta = {
+    'Clients': ['Goldman Sachs', 'Deloitte', 'Bo', 'RBS', 'NHS', 'DirectLine', 'Spotify', 'TFL', 'M&G', 'Prudential']
+}
+print('Using interpolation')
+goldman = (sparta['Clients'][0])
+print(f"{goldman} is a good client")
+delo = (sparta['Clients'][1])
+print(f"{delo} is a nice client")
+bo = (sparta['Clients'][2])
+print(f"{bo} is an amazing client")
+rbs = (sparta['Clients'][3])
+print(f"{rbs} is an excellent client")
+nhs = (sparta['Clients'][4])
+print(f"{nhs} is a chilled client")
+print('---------------------------')
+print('Using concatination')
+direct = (sparta['Clients'][5])
+print((direct) +  " is a cool client")
+spot = (sparta['Clients'][6])
+print((spot) +  " is an enjoyable client")
+tfl = (sparta['Clients'][7])
+print((tfl) +  " is a lovely client")
+mg = (sparta['Clients'][8])
+print((mg) +  " is a really nice client")
+prud = (sparta['Clients'][9])
+print((prud) +  " is a really amazing client")
+
+print('------------------------------------------')
+story = {
+    'beginning': 'Once there was a hare who wanted to race a turtle.',
+    'middle': 'The turtle took its time, while the hare rushed itself towards the finish line.',
+    'end': 'In the end the turtle won. Moral of the story is that the slow and steady wins the race.',
+    'hero': 'Super turtle'
+}
+
+print(story['beginning']),
+print(story['middle']),
+print(story['end']),
+print(story['hero'])
+
+
+
