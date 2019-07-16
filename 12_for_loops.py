6d8f19ea703b4afda9273b0573d08598b95cd1a5
Shavgath Perumal <48409809+shavgath@users.noreply.github.com>
2019-07-16 13:50:25 +0100

    completed for loops using lists

---
+++ 12_for_loops.py
@@ -0,0 +1,65 @@
+import time
+# For loops in Python
+# used to iterate over collections. Collections are a list of dictionaries
+
+# Syntax
+# placeholder is an internal variable that it's scope is limited to the loop
+# for <placeholder> in <list>:
+    # run block of code
+
+
+# x_crazy_landlords = ['Cruella de Vill', 'Donald Duck', 'Popeye the Maltese']
+#
+# counter = 0
+# print('Starting Loop')
+# time.sleep(2)
+# for land_l in x_crazy_landlords:
+#     print('hello')
+#     print(land_l)
+#     print(counter)
+#     counter += 1
+#     time.sleep(3)
+# print('Loop End')
+
+
+# Listing items using numbers while using the for loop
+# x_crazy_landlords = ['Cruella de Vill', 'Donald Duck', 'Popeye the Maltese']
+#
+# counter = 1
+# for land_lord in x_crazy_landlords:
+#     print(counter, '-', land_lord)
+#     counter += 1
+
+
+# list_data = [1, 2, 3, 4, 5]
+# embedded_list = [[1, 2 , 3],[5, 6, 7]]
+#
+# for data in embedded_list:
+#     print(data)
+#     for number in data:
+#         print(number)
+
+# list_of_spartans_a = ['Adam', 'Pratheep', 'Zaid', 'Omid', 'Julian']
+# list_of_spartans_b = ['Naila', 'Payal', 'Daniel', 'Hamza', 'Michael']
+#
+# for sparta_a in list_of_spartans_a:
+#     print(sparta_a + ' is amazing!')
+#     print('-------------------------')
+# for sparta_b in list_of_spartans_b:
+#     print(sparta_b + ' is excellent!')
+#     print('-------------------------')
+
+
+# Find the percentage        # Scores are out of 10
+# list_scores = [1, 10, 3, 4, 5, 6]
+#
+# for num in list_scores:
+#     result_percent = num / 10 * 100
+#     print(result_percent)
+
+# list_embed_scores = [[10, 5 , 2],[3, 4, 6]]
+#
+# for ind_list in list_embed_scores:
+#     print(ind_list)
+#     for num in ind_list:
+#         print(num * 2)
